3540fb248434009ff74dde718cbc3553b26f7891
Toby Varland <toby.varland@varland.com>
2019-02-08 08:43:24 -0500

    Update production server location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "192.168.82.193", user: "deploy", roles: %w{app db web}
+server "journal.annievarland.com", user: "deploy", roles: %w{app db web}
 
 
 # role-based syntax
